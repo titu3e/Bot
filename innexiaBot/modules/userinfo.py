628094bfbabe5f85e35cdd59b3cb0f0226dad429
おい aaman <heyaaman2004@gmail.com>
2021-11-09 12:34:00 +0530

    Update userinfo.py

--- innexiaBot/modules/userinfo.py
+++ innexiaBot/modules/userinfo.py
@@ -409,7 +409,16 @@ def set_about_me(update: Update, context: CallbackContext):
 @run_async
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>★「 Statistics: 」★</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    process = subprocess.Popen(
+        "neofetch --stdout", shell=True, text=True, stdout=subprocess.PIPE
+    )
+    output = process.communicate()[0]
+    stats = (
+        "<b>Current stats:</b>\n"
+        + "\n"
+        + output
+        + "\n".join([mod.__stats__() for mod in STATS])
+    )
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
     update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
 
